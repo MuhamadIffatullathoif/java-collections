f75a38d1a445319abedf4de2cdf6f34d02256fd0
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-03-27 11:13:24 +0700

    07. Collections methods challenge, your own card game

---
+++ src/main/java/org/iffat/games/GameController.java
@@ -0,0 +1,10 @@
+package org.iffat.games;
+
+import org.iffat.games.poker.PokerGame;
+
+public class GameController {
+    public static void main(String[] args) {
+        PokerGame fiveCardDraw = new PokerGame(4, 5);
+        fiveCardDraw.startPlay();
+    }
+}
---
+++ src/main/java/org/iffat/games/poker/Ranking.java
@@ -0,0 +1,15 @@
+package org.iffat.games.poker;
+
+public enum Ranking {
+    NONE,
+    ONE_PIER,
+    TWO_PAIR,
+    THREE_OF_KIND,
+    FULL_HOUSE,
+    FOUR_OF_A_KIND;
+
+    @Override
+    public String toString() {
+        return this.name().replace('_', ' ');
+    }
+}
