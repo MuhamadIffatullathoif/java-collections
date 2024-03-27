cc12122e613b28c5b3c08e23905f179d51f60537
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-03-27 16:51:15 +0700

    11. Introduction to Sets and HashSets

--- src/main/java/org/iffat/sets_and_maps/Main.java
+++ src/main/java/org/iffat/sets_and_maps/Main.java
@@ -1,7 +1,9 @@
 package org.iffat.sets_and_maps;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,6 +12,18 @@ public class Main {
         List<Contact> phones = ContactData.getData("phone");
         printData("Phone List", phones);
         printData("Email List", emails);
+
+        Set<Contact> emailContacts = new HashSet<>(emails);
+        Set<Contact> phoneContacts = new HashSet<>(phones);
+        printData("Phone Contacts", phoneContacts);
+        printData("Email Contacts", emailContacts);
+
+        int index = emails.indexOf(new Contact("Robin Hood"));
+        Contact robinHood = emails.get(index);
+        robinHood.addEmail("Sherwood Forest");
+        robinHood.addEmail("Sherwood Forest");
+        robinHood.replaceEmailIfExists("RHood@sherwoodforest.com", "RHood@sherwoodforest.org");
+        System.out.println(robinHood);
     }
 
     public static void printData(String header, Collection<Contact> contacts) {
