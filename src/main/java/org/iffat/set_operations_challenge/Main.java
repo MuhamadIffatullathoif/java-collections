ddb4879d1b7622e3e63c25b5314eaa51a5a8c2b7
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-03-27 22:14:04 +0700

    13. Code Setup (Task and Task Data)

---
+++ src/main/java/org/iffat/set_operations_challenge/Main.java
@@ -0,0 +1,29 @@
+package org.iffat.set_operations_challenge;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<Task> tasks = TaskData.getTasks("all");
+        sortAndPrint("All Task", tasks);
+
+        Comparator<Task> sortByPriority = Comparator.comparing(Task::getPriority);
+        Set<Task> annsTasks = TaskData.getTasks("Ann");
+        sortAndPrint("Ann's Task", annsTasks, sortByPriority);
+    }
+
+    private static void sortAndPrint(String header, Collection<Task> collection) {
+        sortAndPrint(header, collection, null);
+    }
+
+    private static void sortAndPrint(String header, Collection<Task> collection, Comparator<Task> sorter) {
+        String lineSeparator = "_".repeat(90);
+        System.out.println(lineSeparator);
+        System.out.println(header);
+        System.out.println(lineSeparator);
+
+        List<Task> list = new ArrayList<>(collection);
+        list.sort(sorter);
+        list.forEach(System.out::println);
+    }
+}
