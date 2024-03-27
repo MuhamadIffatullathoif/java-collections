40f1925fb721c0f315730b424972e61aae08368d
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-03-27 08:53:16 +0700

    04. Introduction to java.util.Collections

--- src/main/java/org/iffat/collections_method/Main.java
+++ src/main/java/org/iffat/collections_method/Main.java
@@ -1,10 +1,40 @@
 package org.iffat.collections_method;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<Card> deck = Card.getStandardDeck();
         Card.printDeck(deck);
+
+        Card[] cardArray = new Card[13];
+        Card aceOfHeart = Card.getFaceCard(Card.Suit.HEART, 'A');
+        Arrays.fill(cardArray, aceOfHeart);
+        Card.printDeck(Arrays.asList(cardArray), "Aces of Hearts", 1);
+
+        List<Card> cards = new ArrayList<>(52);
+        Collections.fill(cards, aceOfHeart);
+        System.out.println(cards);
+        System.out.println("card.size() = " + cards.size());
+
+        List<Card> acesOfHeart = Collections.nCopies(13, aceOfHeart);
+        Card.printDeck(acesOfHeart, "Aces of Hearts", 1);
+
+        Card kingOfClubs = Card.getFaceCard(Card.Suit.CLUB, 'K');
+        List<Card> kingsOfClubs = Collections.nCopies(13, kingOfClubs);
+        Card.printDeck(kingsOfClubs, "Kings of Clubs", 1);
+
+        Collections.addAll(cards, cardArray);
+        Collections.addAll(cards, cardArray);
+        Card.printDeck(cards, "Card Collection with Aces added", 2);
+
+        Collections.copy(cards, kingsOfClubs);
+        Card.printDeck(cards, "Card Collection wtih Kings copies", 2);
+
+        cards = List.copyOf(kingsOfClubs);
+        Card.printDeck(cards, "List Copy of Kings", 1);
     }
 }
