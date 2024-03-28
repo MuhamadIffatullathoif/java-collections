797a2cbf9e78dfdee7a7af008748d5fb1ee7bf0a
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-03-28 08:19:22 +0700

    16. TreeSet, Closest match and subset methods

--- src/main/java/org/iffat/set_operations_challenge/TreeSetMain.java
+++ src/main/java/org/iffat/set_operations_challenge/TreeSetMain.java
@@ -43,9 +43,51 @@ public class TreeSetMain {
         System.out.println("-".repeat(90));
 
         NavigableSet<Contact> copiedSet = new TreeSet<>(fullSet);
-        System.out.println("First element = " +copiedSet.pollFirst());
-        System.out.println("Last element = " +copiedSet.pollLast());
+        System.out.println("First element = " + copiedSet.pollFirst());
+        System.out.println("Last element = " + copiedSet.pollLast());
         copiedSet.forEach(System.out::println);
         System.out.println("-".repeat(90));
+
+        Contact daffy = new Contact("Daffy Duck");
+        Contact daisy = new Contact("Daisy Duck");
+        Contact snoopy = new Contact("Snoopy");
+        Contact archie = new Contact("Archie");
+
+        for (Contact contact : List.of(daffy, daisy, last, snoopy)) {
+            System.out.printf("ceiling(%s)=%s%n", contact.getName(), fullSet.ceiling(contact));
+            System.out.printf("higher(%s)=%s%n", contact.getName(), fullSet.higher(contact));
+        }
+        System.out.println("-".repeat(90));
+
+        for (Contact contact : List.of(daffy, daisy, first, archie)) {
+            System.out.printf("floor(%s)=%s%n", contact.getName(), fullSet.floor(contact));
+            System.out.printf("lower(%s)=%s%n", contact.getName(), fullSet.lower(contact));
+        }
+        System.out.println("-".repeat(90));
+
+        NavigableSet<Contact> descendingSet = fullSet.descendingSet();
+        descendingSet.forEach(System.out::println);
+        System.out.println("-".repeat(90));
+
+        Contact lastContact = descendingSet.pollLast();
+        System.out.println("Removed " + lastContact);
+        descendingSet.forEach(System.out::println);
+        System.out.println("-".repeat(90));
+        fullSet.forEach(System.out::println);
+        System.out.println("-".repeat(90));
+
+        Contact marion = new Contact("Maid Marion");
+        var headSet = fullSet.headSet(marion, true);
+        headSet.forEach(System.out::println);
+        System.out.println("-".repeat(90));
+
+        var tailSet = fullSet.tailSet(marion, false);
+        tailSet.forEach(System.out::println);
+        System.out.println("-".repeat(90));
+
+        Contact linus = new Contact("Linus Van Pelt");
+        // var subset = fullSet.subSet(linus, marion);
+        var subset = fullSet.subSet(linus, false, marion, true);
+        subset.forEach(System.out::println);
     }
 }
